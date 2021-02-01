1a836e95a731a1b80fb057e0a4c00e3ec0a949e0
Gunnar Morling <gunnar.morling@googlemail.com>
2021-02-01 21:21:46 +0100

    📝 README update

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Java 16 Unix Domain Socket PoC
 
 A proof-of-concept for using Java 16's Unix domain socket support (JEP 380) with the Vert.x Postgres driver.
+See [this blog post](https://www.morling.dev/blog/talking-to-postgres-through-java-16-unix-domain-socket-channels/) for some background.
 
 ## Build
 
 This project requires OpenJDK 16 or later for its build.
 Apache Maven is used for the build.
-Run the following to build the project (requires Postgres to be running locally):
+Run the following to build the project
+(requires Postgres to be running locally,
+with a database `test_db`, and a user `test_user` with password `4fafsqfa98()d#er`;
+see [here](https://www.postgresql.org/download/linux/redhat/) and [here](https://computingforgeeks.com/how-to-install-postgresql-on-fedora/) for installation instructions for Fedora):
 
 ```shell
-mvn clean install
+mvn clean verify
 ```
 
 ## License
--- src/test/java/dev/morling/demos/uds/UnixDomainSocketTest.java
+++ src/test/java/dev/morling/demos/uds/UnixDomainSocketTest.java
@@ -7,8 +7,15 @@ package dev.morling.demos.uds;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import io.vertx.sqlclient.Row;
@@ -16,6 +23,44 @@ import io.vertx.sqlclient.RowSet;
 
 
 public class UnixDomainSocketTest {
+
+    @Before
+    public void setupDatabase() throws Exception {
+        String url = "jdbc:postgresql://localhost/test_db";
+        Properties props = new Properties();
+        props.setProperty("user", "test_user");
+        props.setProperty("password", "4fafsqfa98()d#er");
+
+        Connection conn = DriverManager.getConnection(url, props);
+
+        var ddl = """
+            create table customer.Customer (
+                id int8 not null,
+                name varchar(255),
+                open_limit int8 not null,
+                primary key (id)
+            )
+        """;
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate("drop table if exists customer.customer");
+            stmt.executeUpdate("drop schema if exists customer");
+            stmt.executeUpdate("create schema customer");
+            stmt.executeUpdate(ddl);
+            stmt.executeUpdate("insert into customer.customer (id, name, open_limit) values (1, 'Bob', 40000)");
+        }
+            catch (SQLException e) {
+                throw new RuntimeException(e);
+        }
+
+        try (Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT name FROM customer.customer");) {
+
+            while (rs.next()) {
+                System.out.print("Column 1 returned ");
+                System.out.println(rs.getString(1));
+            }
+        }
+    }
 
     @Test
     public void shouldObtainResult() throws Exception {
@@ -33,6 +78,8 @@ public class UnixDomainSocketTest {
                      );
                 }
                 else {
+                    ar.cause().printStackTrace();
+
                     fail(ar.cause().getMessage());
                 }
 
